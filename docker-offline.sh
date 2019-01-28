ab315377876fbf4afe2291598c346807bfd0fbba
user <user@BigHero6.rudel>
2019-01-28 20:33:59 +0100

    Initial Commit

---
+++ docker-offline.sh
@@ -0,0 +1,35 @@
+#/bin/bash
+IMAGEDIR="offline-images"
+case $1 in 
+	"save")
+		mapfile -t images < <(cat docker-compose.yml | grep image | cut  -d ':' -f2,3 | cut -c2-)
+		
+		if [ ${#images[@]} -eq 0 ]; then
+			exit 1
+		fi
+		
+		if [ ! -d "$IMAGEDIR" ]; then
+			echo "Creating $IMAGEDIR"	
+			mkdir $IMAGEDIR
+		fi
+
+		for image in "${images[@]}"
+		do
+			echo $image
+			docker save $image > $IMAGEDIR/$image.tar
+		done
+		;;
+
+	"load")
+		mapfile -t images < <(ls $IMAGEDIR | grep .tar)
+		for image in "${images[@]}"
+		do
+			echo $image
+			docker load < $IMAGEDIR/$image
+		done
+		;;
+	*)
+		echo $0 "[ load | save ]"
+		;;
+esac
+
